d72e333cc8968cdc5d37af2054a272a1808fac51
zyhvv2 <ttoopp001@163.com>
2025-01-26 12:19:53 +0800

    Add files via upload

---
+++ Service/store/page/store-app-1.bat
@@ -0,0 +1,8 @@
+set store-view-app-name=DTool
+set store-view-app-size=小于1Mb
+set store-view-app-provider=ZOS
+set store-view-app-devlopment=ZOS
+set store-view-app-exercise=
+set store-view-app-updatalog=第一代已发布
+set store-view-app-preview=安卓手机调试工具
+set store-view-app-supportv=4

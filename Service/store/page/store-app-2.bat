29435da43cacf5b54dbf5949d5178479b26ba9fa
zyhvv2 <ttoopp001@163.com>
2025-02-15 12:51:28 +0800

    Add files via upload

--- Service/store/page/store-app-2.bat
+++ Service/store/page/store-app-2.bat
@@ -1,13 +1,13 @@
 ::app info
-set store-view-app-name=DTool
+set store-view-app-name=TeleMsg
 set store-view-app-version=1.0
 set store-view-app-size=小于1Mb
-set store-view-app-pn=com.zos.dtool
+set store-view-app-pn=com.zos.telemsgs
 set store-view-app-provider=ZOS
 set store-view-app-devlopment=ZOS
 set store-view-app-exercise=---
 set store-view-app-updatalog=第一代已发布
-set store-view-app-preview=安卓手机调试工具
+set store-view-app-preview=通信工具
 set store-view-app-supportv=4
 ::调用appview
 call core\Appview
